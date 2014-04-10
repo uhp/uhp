708957e441de158f7003465d734ae7c14130e23f
qus-jiawei <qus-jiawei@gmail.com>
2014-04-10 10:01:16 +0800

    add init function and text

--- uhpcommon/model/alarm.py
+++ uhpcommon/model/alarm.py
@@ -15,11 +15,22 @@ class Alarm(BASE, UHPBase):
 
     id= Column(Integer, primary_key=True)
     name= Column(String(30), nullable=False, unique=True)
+    #判断函数,可设定传入的参数。传入的参数是 ganglia收集的指标名称
+    #例如  max( load_one , 10 ) 则用这两个作为参数进行调用 
+    #返回一个二元组  二元组第一项是报警等级 OK WARN ERROR 第二项是报警信息
     expression= Column(String(250), nullable=False)
+    #回调函数  sendmail 调用参数是 expression的返回的二元组
     callback= Column(String(250), nullable=False)
+    #为* 或者为 机器名
     host = Column(String(30),nullable=False)
 
     HOST_ALL = "*"
+    
+    def __init__(self, name, expression, callback, host=HOST_ALL ):
+        self.name = name
+        self.expression = expression
+        self.callback = callback
+        self.host = host
     
 class AlarmAssist(BASE, UHPBase):
     
@@ -28,3 +39,8 @@ class AlarmAssist(BASE, UHPBase):
     id= Column(Integer, primary_key=True)
     name= Column(String(30), nullable=False, unique=True)
     value= Column(Text, nullable=False)
+
+    def __init__(self, name, value ):
+        self.name = name
+        self.value = value
+        
