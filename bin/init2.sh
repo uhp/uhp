b8348644de4a79f272ae68f7f7523b7d75d8bce5
zhaigy <hontlong@gmail.com>
2014-04-10 04:00:39 -0700

    分离出添加，删除机器的页面

--- bin/init2.sh
+++ bin/init2.sh
@@ -2,14 +2,46 @@
 set -e
 
 if [ "$UHP_HOME" == "" ] ; then 
-    echo "UHP_HOME is not set; please insert export UHP_HOME=$HOME/uhp into your env" ;
-    exit 1; 
+    DIR=$(cd $(dirname "$0");cd ..; pwd)
+    echo "export UHP_HOME=$DIR" >> ~/.bash_profile
+    echo "export ANSIBLE_CONFIG=\$HUP_HOME/conf/ansible.cfg" >> ~/.bash_profile
+    echo "export PATH=\$UHP_HOME/bin:\$PATH" >> ~/.bash_profile
+    export UHP_HOME=$DIR
 fi
 
+cd $UHP_HOME
+chmod a+x bin/*.sh
+
+export VIRTUAL_ENV="$UHP_HOME/vpy" 
+
+#python lib/virtualenv-*.py --no-site-packages $VIRTUAL_ENV
+#echo "export PATH=$VIRTUAL_ENV/bin:\$PATH" >> ~/.bash_profile
+#export PATH=$VIRTUAL_ENV/bin:$PATH
+
+sudo yum install -y make python2-devel mysql-devel
+
+PIP="$VIRTUAL_ENV/bin/pip"
+
+coms=("snakemq"
+    "sqlalchemy"
+    "tornado"    
+    "threadpool"
+    "callbacks"
+    "pyyaml"
+    "python-daemon"
+    "mysql-python"
+    "ansible"
+    "lockfile")
+
+for com in ${coms[@]}; do
+    echo "--------$com--------"
+    $PIP install $com
+done 
+
+exit 0
 sudo yum install -y python-pip rpm-build make python2-devel mysql-devel
 sudo pip-python install -U pip
 sudo yum install -y ansible
-sudo yum install -y rrdtool rrdtool-python
 #check ansible 
 #检查ansible安装成功
 #RE=`ansible  --version|grep ansible|wc -l`
