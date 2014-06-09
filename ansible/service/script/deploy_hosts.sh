96451ce4be36730028263be99dbe33ecf1b80f51
qus-jiawei <qus-jiawei@gmail.com>
2014-06-09 18:05:29 +0800

    add sync host feature

---
+++ ansible/service/script/deploy_hosts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#提供给web调用
+#接收参数 host(要更新的机器) auto_add(要添加的host对)
+#配合ansible/service/deploy_hosts.yml 和 ansible/service/replace_file.sh 使用
+#本脚本直接调用ansible-playbook ansible/service/deploy_hosts.yml
+
+host=$1
+auto_add=$2
+
+cd $UHP_HOME/ansible/service/
+ansible-playbook -i $UHP_HOME/inventor/mysqlinventory.py deploy_hosts.yml -e "{'WANT_HOST':'$host','AUTO_ADD':'$auto_add'}"
